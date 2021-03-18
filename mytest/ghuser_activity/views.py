df0675a60e1112461aa739713d263ec77e29eb82
mign0n <56875363+mign0n@users.noreply.github.com>
2021-03-18 18:15:25 +0500

    Added form to enter github username.

--- mytest/ghuser_activity/views.py
+++ mytest/ghuser_activity/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 
+from ghuser_activity.forms import UserNameForm
+
 
 def index(request):
+    username = 'Unknown'
+    if request.method == 'POST':
+        form = UserNameForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+    else:
+        form = UserNameForm()
     return render(
         request,
         'ghuser_activity/index.html',
-        {'title': 'Get Github user activity info.'}
+        {
+            'title': 'Get Github user activity info.',
+            'form': form,
+            'username': username,
+        },
     )
