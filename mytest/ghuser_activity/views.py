a58e8d82b23f16a1f69c278f285065219545609e
mign0n <56875363+mign0n@users.noreply.github.com>
2021-03-18 16:44:12 +0500

    Added main page template.

--- mytest/ghuser_activity/views.py
+++ mytest/ghuser_activity/views.py
@@ -1,6 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('<h3>Functionality is coming soon.</h3>')
+    return render(
+        request,
+        'ghuser_activity/index.html',
+        {'title': 'Get Github user activity info.'}
+    )
